f69c7c96aae1cda77fbd1b8baf8fb6421f15ed5f
sugarshin <shinsugar@gmail.com>
2018-08-14 19:11:14 +0900

    Fix public path

--- src/server/dev.tsx
+++ src/server/dev.tsx
@@ -19,7 +19,7 @@ server.use(webpackDevMiddleware(compiler, {
 }))
 server.use(webpackHotMiddleware(compiler))
 
-server.use(express.static(resolve(__dirname, 'dev')))
+server.use(express.static(resolve(process.cwd(), 'dev')))
 server.use('/api', apiRoutes) // should be above top more than '/'
 server.use('/', routes)
 server.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -8,7 +8,7 @@ import apiRoutes from '../routes/api'
 const server = express()
 const { PORT = 3000 } = process.env
 
-server.use(express.static(resolve(__dirname, 'public')))
+server.use(express.static(resolve(process.cwd(), 'public')))
 server.use('/api', apiRoutes) // should be above top more than '/'
 server.use('/', routes)
 server.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
