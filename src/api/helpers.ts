5c3b9b8a37a7795e4ee358c001eb2f94b8948712
sugarshin <shinsugar@gmail.com>
2018-08-19 17:18:58 +0900

    Remove micro-errors

--- src/api/helpers.ts
+++ src/api/helpers.ts
@@ -1,7 +1,28 @@
-/// <reference path='../index.d.ts' />
+export class ProblemError extends Error {
+  public readonly name: 'ProblemError'
+  constructor(
+    public status: number,
+    public title: string,
+    public originalError?: Error,
+    public type: string = 'about:blank',
+    public details?: { [key: string]: any }
+  ) {
+    super(title)
+    Object.setPrototypeOf(this, ProblemError.prototype)
+  }
+}
 
-import { createError } from 'micro-errors'
+export const createError = (
+  statusCode: number,
+  title: string,
+  original?: Error,
+  type?: string,
+  details?: { [key: string]: any }
+): ProblemError => {
+  const error = new ProblemError(statusCode, title, original, type, details)
+  return error
+}
 
-export const createInternalServerError = (error: Error): Error => {
+export const createInternalServerError = (error: Error): ProblemError => {
   return createError(500, 'Internal Server Error', error)
 }
