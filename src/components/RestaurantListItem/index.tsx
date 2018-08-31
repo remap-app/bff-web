14d446196e70dbf9a4f4bd3e0a2a054dcfb4b4a9
sugarshin <shinsugar@gmail.com>
2018-08-31 20:16:39 +0900

    Fix link

--- src/components/RestaurantListItem/index.tsx
+++ src/components/RestaurantListItem/index.tsx
@@ -7,7 +7,7 @@ import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
-import CardActionArea from '@material-ui/core/CardActionArea'
+import _CardActionArea from '@material-ui/core/CardActionArea'
 import Typography from '@material-ui/core/Typography'
 import { MediaPlaceholder } from '../MediaPlaceholder'
 import { IData as IRestaurant } from '../../modules/restaurant'
@@ -19,23 +19,22 @@ export interface IProps {
   component?: string;
 }
 
+const CardActionArea: any = _CardActionArea // TODO
 export const RestaurantListItem = withStyle(s)((props: IProps): JSX.Element => {
   return (
     <Card className={classnames(s.root, props.className)} component={props.component}>
-      <CardActionArea className={s.action}>
-        <Link to={`/restaurants/${props.restaurant.id}`} className={s.link}>
-          <CardHeader
-            avatar={<Avatar>{props.restaurant.name.slice(0, 1)}</Avatar>}
-            title={props.restaurant.name}
-            subheader={props.restaurant.catchphrase || '-'}
-          />
-          {props.restaurant.images[0]
-            ? <CardMedia image={props.restaurant.images[0]} className={s.media} />
-            : <MediaPlaceholder />}
-          <CardContent>
-            <Typography component='p'>{props.restaurant.description || '-'}</Typography>
-          </CardContent>
-        </Link>
+      <CardActionArea className={`${s.action} ${s.link}`} component={Link} to={`/restaurants/${props.restaurant.id}`}>
+        <CardHeader
+          avatar={<Avatar>{props.restaurant.name.slice(0, 1)}</Avatar>}
+          title={props.restaurant.name}
+          subheader={props.restaurant.catchphrase || '-'}
+        />
+        {props.restaurant.images[0]
+          ? <CardMedia image={props.restaurant.images[0]} className={s.media} />
+          : <MediaPlaceholder />}
+        <CardContent>
+          <Typography component='p'>{props.restaurant.description || '-'}</Typography>
+        </CardContent>
       </CardActionArea>
     </Card>
   )
